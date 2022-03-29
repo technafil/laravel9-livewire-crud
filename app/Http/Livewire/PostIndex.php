ae4b66ec858a65f5ca1e937147c5ee9ce04afc72
nafil <tech.nafil@gmail.com>
2022-03-29 21:32:46 +0530

    first commit

---
+++ app/Http/Livewire/PostIndex.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class PostIndex extends Component
+{
+    public function render()
+    {
+        return view('livewire.post-index');
+    }
+}
